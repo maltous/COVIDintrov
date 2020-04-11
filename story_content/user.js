585bfe57daed9a55bbd572662ece735986033800
nsddksmc <40565225+nsddksmc@users.noreply.github.com>
2020-04-11 17:13:03 +0300

    COVIDintrov

---
+++ story_content/user.js
@@ -0,0 +1,47 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "6U3KDQ2qrER":
+        Script1();
+        break;
+      case "5xoDfw62K5u":
+        Script2();
+        break;
+      case "5W4Su5cMI3a":
+        Script3();
+        break;
+      case "5dtsLExHcjo":
+        Script4();
+        break;
+      case "6ZKUEx599F5":
+        Script5();
+        break;
+  }
+}
+
+function Script1()
+{
+  $("#tab-customlink").hide();
+}
+
+function Script2()
+{
+  $("#tab-customlink").hide();
+}
+
+function Script3()
+{
+  $("#tab-customlink").hide();
+}
+
+function Script4()
+{
+  $("#tab-customlink").hide();
+}
+
+function Script5()
+{
+  window.print();
+}
+
